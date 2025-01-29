e20ccb28dc93fec23870c22e772ab677cef1fb3b
Ali Abooof <aliabooof@outlook.com>
2025-01-29 10:41:24 +0200

    the properties for the application user

--- Authentication & Authorization/Models/ApplicationUser.cs
+++ Authentication & Authorization/Models/ApplicationUser.cs
@@ -1,8 +1,20 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace Authentication___Authorization.Models
 {
     public class ApplicationUser :IdentityUser
     {
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; } = default!;
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; } = default!;
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
     }
 }
