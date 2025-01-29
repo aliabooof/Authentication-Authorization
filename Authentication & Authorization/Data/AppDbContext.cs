b943860b9c073225d10ae7ef8fe9559c97bf1e28
Ali Abooof <aliabooof@outlook.com>
2025-01-29 10:50:49 +0200

    configuring the appdbcontext add services using dependency injection add first migration updating the data base

---
+++ Authentication & Authorization/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Authentication___Authorization.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Authentication___Authorization.Data
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
